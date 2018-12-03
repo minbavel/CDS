f5fe50a35a9a754a164f49bc78faefeb482e8630
minbavel <44683505+minbavel@users.noreply.github.com>
2018-12-03 18:21:57 -0500

    Add files via upload

---
+++ login/foodpoison.php
@@ -0,0 +1,38 @@
+<?php include("includes/a_config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body>
+
+<?php include("includes/design-top.php");?>
+<?php include("includes/navigation.php");?>
+
+<div class="container" id="main-content">
+	
+    <p>The Disease Might Be <b>Food Posion</b></p>
+    <p><h3>Overview</h3>
+
+    <h4>Symptoms of Dog Poisoning</h4><br>
+
+The many different types of dog poisoning have various symptoms. We all know our dogs get into things they aren’t supposed to, so it’s our job to keep harmful objects away from them. Below are the different types of symptoms associated with dog poisoning.<br>
+    <h4>Irregular Heart beats From Dog Poisoning</h4><br>
+
+Dogs with irregular heart rhythms and cardiac symptoms have most likely gotten into a medication or plant. This includes jimson weed, kalanchoe, milkweed, mountain laurel and oleander.<br>
+    <h4>Kidney Failure From Poisoning</h4><br>
+
+Antifreeze poisoning can cause your dog kidney failure and the inability to produce urine. Plants can also cause kidney damage. These types of plants include dieffenbachia, Easter lily, caladium, pigweed and philodendron.<br>
+<p>Liver Damage From Dog Poisoning
+
+Medications like acetaminophen and plants such as tansy ragwort or rattlebox can cause liver damage.
+Loss of Blood From Dog Poisoning
+
+If your dog has bruising, blood in their stool, nosebleeds or anemia they have most likely gotten into rat or mouse poison. However, if they’ve gotten into your garden or kitchen and eaten too much onion, garlic, sweet clover or bracken fern they could also suffer from anemia and could even die.
+Neurological Symptoms From Dog Poisoning
+
+    Dogs suffering from seizures and other neurological symptoms have possibly ingested one of many things. This list is long and includes the following: antidepressants, alcohol, aspirin, drain cleaners, dishwasher soap, gasoline, marijuana, flea repellents, tobacco, furniture polish and strychnine. Exposure to, or bites from, poisonous animals can cause dogs to seize as well. These animals include certain breeds of snakes, spiders, toads and frogs. Specifically, you’ll want to look out for Florida marine toads, Coral snakes, Colorado River toads and brown recluse spiders. Plants can cause neurological symptoms as well, this includes buckeyes and horse chestnuts.</p>
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
